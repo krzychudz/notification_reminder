9bc5f35b247f682f073149725c4a95a02e252bf6
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2023-01-14 16:23:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/notificationremindermt3/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.notificationremindermt3
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.notificationremindermt3.ui.theme.NotificationReminderMt3Theme
+
+@OptIn(ExperimentalMaterial3Api::class)
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotificationReminderMt3Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(
+                        topBar = { TopAppBar(title = { Text(text = "First Page") }) },
+                        
+                        content = {
+                            Greeting("Hello")
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}

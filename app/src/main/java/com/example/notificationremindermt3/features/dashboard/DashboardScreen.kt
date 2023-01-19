2a22c0367eab2308645d63c18e02caab33d9f0ef
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2023-01-19 21:37:10 +0100

    Remove unused import

--- app/src/main/java/com/example/notificationremindermt3/features/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/example/notificationremindermt3/features/dashboard/DashboardScreen.kt
@@ -1,23 +1,10 @@
 package com.example.notificationremindermt3.features.dashboard
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowRight
-import androidx.compose.material.icons.rounded.ArrowForward
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import com.example.notificationremindermt3.features.dashboard.composable.NotificationItem
 

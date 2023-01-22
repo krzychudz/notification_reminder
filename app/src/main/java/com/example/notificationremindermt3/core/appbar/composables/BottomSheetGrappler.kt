2f5215e0b4da9f9bb74566667a705675c7cf4e51
Krzysztof Dziedzic <krzysztof.dziedzic175@gmail.com>
2023-01-22 19:26:44 +0100

    Move components to separated composables

--- app/src/main/java/com/example/notificationremindermt3/core/appbar/composables/BottomSheetGrappler.kt
+++ app/src/main/java/com/example/notificationremindermt3/core/appbar/composables/BottomSheetGrappler.kt
@@ -1,7 +1,7 @@
 package com.example.notificationremindermt3.core.appbar.composables
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,12 +11,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun BottomSheetGrappler() {
+fun BottomSheetGrappler(modifier: Modifier = Modifier) {
     Box(
-        modifier = Modifier
+        modifier = modifier
             .width(32.dp)
             .height(4.dp)
-            .background(color = Color(0xFF79747E), shape = RoundedCornerShape(100)),
-        content = {}
+            .background(color = Color(0x6679747E), shape = RoundedCornerShape(100)),
+        content = {
+        }
     )
 }
--- app/src/main/java/com/example/notificationremindermt3/features/new_notification/AddNotificationBottomSheet.kt
+++ app/src/main/java/com/example/notificationremindermt3/features/new_notification/AddNotificationBottomSheet.kt
@@ -1,14 +1,16 @@
 package com.example.notificationremindermt3.features.new_notification
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.example.notificationremindermt3.core.appbar.composables.BottomSheetGrappler
 
@@ -17,27 +19,49 @@ import com.example.notificationremindermt3.core.appbar.composables.BottomSheetGr
 fun AddNotificationBottomSheetBody() {
     var notificationName by remember { mutableStateOf("") }
 
-    Box(
-        Modifier
+    Column(
+        modifier = Modifier
             .fillMaxWidth()
-            .height(200.dp)
-            .padding(20.dp)
+            .padding(32.dp)
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            BottomSheetGrappler()
-            Spacer(modifier = Modifier.height(16.dp))
-            TextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = notificationName,
-                onValueChange = { notificationName = it },
-                maxLines = 1,
-                label = { Text(text = "Notification Name")  }
-            )
-            Spacer(modifier = Modifier.height(16.dp))
+        BottomSheetGrappler(modifier = Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = notificationName,
+            onValueChange = { notificationName = it },
+            maxLines = 1,
+            label = { Text(text = "Notification Name") },
+            colors = TextFieldDefaults.textFieldColors(containerColor = Color.Transparent)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Notification dates")
+        Spacer(modifier = Modifier.height(8.dp))
+        CircularIconButton(image = Icons.Rounded.Add, contentDescription = "Add notification date", onClick = {})
+        SubmitButton(modifier = Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.height(8.dp))
+    }
+}
 
-        }
+@Composable
+fun CircularIconButton(onClick: () -> Unit, image: ImageVector, contentDescription: String) {
+    IconButton(
+        onClick = onClick,
+        modifier = Modifier.background(
+            color = MaterialTheme.colorScheme.surface,
+            shape = CircleShape
+        )
+    ) {
+        Icon(image, contentDescription)
+    }
+}
+
+@Composable
+fun SubmitButton(modifier: Modifier = Modifier) {
+    ElevatedButton(
+        onClick = { /*TODO*/ },
+        modifier = modifier
+    ) {
+        Text(text = "Add Notification")
     }
 }
